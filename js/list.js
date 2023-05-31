7b50b2f5957f1b0964a62ecc702fa05c76ad5810
justanormaldog-disc <jackson.wilmot@icloud.com>
2023-06-01 07:10:26 +1000

    Initial commit

---
+++ js/list.js
@@ -0,0 +1,55 @@
+var input_aria = document.getElementById("input-aria");
+var input_button = document.getElementById("radius");
+var input_qnty = document.getElementById("qnty-aria");
+var table = document.getElementById('result');
+var table_element = document.getElementsByTagName("tr");
+function initList() {
+    shop_list = [];
+    console.log(input_aria);
+}
+
+function CAUitem(itemName, quantity) {
+    for (let i = 0; i < shop_list.length; i++) {
+      if (shop_list[i][0].toLowerCase() === itemName.toLowerCase()) {
+        shop_list[i][1] += quantity;
+        displayArray(shop_list);
+        return;
+      }
+    }
+    // If item is not found, add it to the shopping list
+    shop_list.push([itemName, quantity]);
+    displayArray(shop_list);
+  }
+
+initList();
+input_button.addEventListener("click", function() {
+if (input_aria.value !== "" && input_qnty.value !=="") {
+CAUitem(input_aria.value,parseInt(input_qnty.value,10));
+//clears textbox(s)
+input_aria.value = "";
+input_qnty.value = "";
+}});
+
+//concentates and displays the array
+function displayArray(arr) {
+  // Clear existing rows with id="tab"
+  var tabRows = document.querySelectorAll('#result tr[id="tab"]');
+  tabRows.forEach(function(row) {
+    row.parentNode.removeChild(row);
+  });
+
+  for (var i = 0; i < arr.length; i++) {
+    //shorthand for array
+    var item = arr[i];
+    //item name
+    var name = item[0];
+    //quantity value
+    var value = item[1];
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    row.setAttribute("id", "tab");
+    cell1.innerHTML = name;
+    cell2.innerHTML = value;
+  }
+}
---
+++ js/main.js
@@ -0,0 +1,77 @@
+var user,comment,item;
+var input_aria = document.getElementById("input-aria");
+var input_button = document.getElementById("input-form");
+user = 'Jackson';
+
+//list of comments
+const COMMENTS = [
+"Keep on adding!",
+"Dont stop the clock!",
+"Longer and longer and longer the list shall grow...",
+"A list for everyone! Even your pets!",
+"A looooooooooooooooooooooong list",
+"Could a Dachshund be this long?",
+"Out of comments...",
+"Unleash your shopping superpowers and keep on adding to the list!",
+"Don't let the shopping clock stop ticking! Add more items!",
+"Never heard of too much?",
+"Remember, this list isn't just for you, it's for your pets too! A shopping extravaganza for everyone!",
+"Expand the shopping horizons! Keep those items coming!",
+"Unlock the infinite potential of your shopping list and let it grow endlessly!",
+"Embrace the shopping marathon and make the list go on and on!",
+"From essentials to extravagance, this list has room for it all!",
+"Unleash your inner shopaholic and let this list reach new lengths!",
+"Is there a limit to shopping desires? Not on this list!",
+"Overflow this list with all your shopping dreams and desires!",
+"Remember, this list isn't just a collection of items, it's a testament to your shopping prowess!"
+]
+//list of placeholders
+aria = [
+"Meat Pie",
+"Ice-Cream",
+"Pizza",
+"Cake",
+"Broccoli",
+"Carrots",
+"Biscuits",
+"Beetroot",
+"Maple Syrup",
+"Pancake Mix",
+"Bread",
+"Rice",
+"Tamato Sauce",
+"Milk",
+"Potatoes",
+"Eggs",
+"Chicken",
+"Butter",
+"Canned Tuna",
+"Noodles",
+"Meatballs",
+"Sugar & Salt"
+]
+function init() {
+// run functions and other
+document.getElementById("name").innerHTML = 'Hello, ' + user ;
+input_aria.placeholder = aria[Math.floor(Math.random()*aria.length)] + "...";
+document.getElementById("comment").innerHTML = item;
+createComment();
+
+
+ }
+function createComment() {
+    //grabs a random comment and displays it
+    item = COMMENTS[Math.floor(Math.random()*COMMENTS.length)];
+    document.getElementById("comment").innerHTML = item;
+}
+
+/*
+===== ========    ===     =====    ========
+==       ==      == ==    ==   ==     ==
+=====    ==     ==   ==   =====       ==
+  ===    ==    =========  ==   ==     ==
+=====    ==    ==      == ==    ==    ==
+*/
+//init
+
+init()
