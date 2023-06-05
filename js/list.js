bb6fbda38cec2ffb009ac671db1e24c6c7b77b43
justanormaldog-disc <129561053+justanormaldog-disc@users.noreply.github.com>
2023-06-05 20:49:01 +1000

    Update list.js

--- js/list.js
+++ js/list.js
@@ -2,72 +2,83 @@ var input_aria = document.getElementById("input-aria");
 var input_button = document.getElementById("radius");
 var input_qnty = document.getElementById("qnty-aria");
 var table = document.getElementById('result');
-var table_element = document.getElementsByTagName("tr");
 
 var shop_list = [];
 
 function initList() {
-  shop_list = [];
+    shop_list = [];
 }
 
 function addItem(itemName, quantity) {
-  var lowercaseItemName = itemName.toLowerCase();
-  var foundItem = false;
+    var lowercaseItemName = itemName.toLowerCase();
+    var foundItem = false;
 
-  for (var i = 0; i < shop_list.length; i++) {
-    var item = shop_list[i];
-    if (item[0].toLowerCase() === lowercaseItemName) {
-      item[1] += quantity;
-      foundItem = true;
-      break;
+    for (var i = 0; i < shop_list.length; i++) {
+        var item = shop_list[i];
+        if (item[0].toLowerCase() === lowercaseItemName) {
+            item[1] += quantity;
+            foundItem = true;
+            break;
+        }
     }
-  }
 
-  if (!foundItem) {
-    shop_list.push([itemName, quantity]);
-  }
+    if (!foundItem) {
+        shop_list.push([itemName, quantity]);
+    }
 
-  displayArray(shop_list);
+    displayArray(shop_list);
 }
 
 function displayArray(arr) {
-  while (table.rows.length > 1) {
-    table.deleteRow(1);
-  }
+    table.innerHTML = ''; // Clear the table
 
-  for (var i = 0; i < arr.length; i++) {
-    var item = arr[i];
-    var name = item[0];
-    var value = item[1];
-    var row = table.insertRow(-1);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
-    var cell3 = row.insertCell(2);
-    var del = document.createElement("input");
-    del.type = "button";
-    del.className = "del-btn";
-    del.value = 'Delete';
-    cell1.innerHTML = name;
-    cell2.innerHTML = value;
-    cell3.appendChild(del);
-    del.setAttribute("data-index", i);
-    del.addEventListener("click", function() {
-      var index = parseInt(this.getAttribute("data-index"));
-      var row = this.parentNode.parentNode;
-      if (row.tagName === 'TR') {
-        table.deleteRow(row.rowIndex);
-        shop_list.splice(index, 1);
-        updateIndices();
-      }
-    });
-  }
-
-  updateIndices();
+    for (var i = 0; i < arr.length; i++) {
+        var item = arr[i];
+        var name = item[0];
+        var value = item[1];
+        var row = table.insertRow(-1);
+        var cell1 = row.insertCell(0);
+        var cell2 = row.insertCell(1);
+        var cell3 = row.insertCell(2);
+        var del = document.createElement("input");
+        del.type = "button";
+        del.className = "del-btn";
+        del.value = 'Delete';
+        cell1.innerHTML = name;
+        cell2.innerHTML = value;
+        cell3.appendChild(del);
+        del.setAttribute("data-index", i);
+        del.addEventListener("click", function() {
+            var index = parseInt(this.getAttribute("data-index"));
+            shop_list.splice(index, 1);
+            displayArray(shop_list);
+        });
+    }
 }
 
-function updateIndices() {
-  var delButtons = document.getElementsByClassName('del-btn');
-  for (var i = 0; i < delButtons.length; i++) {
-    delButtons[i].setAttribute("data-index", i);
-  }
+function init() {
+    document.getElementById("name").innerHTML = 'Hello, ' + user;
+    input_aria.placeholder = aria[Math.floor(Math.random() * aria.length)] + "...";
+    document.getElementById("comment").innerHTML = item;
+    createComment();
 }
+
+function createComment() {
+    item = COMMENTS[Math.floor(Math.random() * COMMENTS.length)];
+    document.getElementById("comment").innerHTML = item;
+}
+
+init();
+initList();
+
+// Event listener for the button click
+input_button.addEventListener("click", function() {
+    var itemName = input_aria.value;
+    var quantity = parseInt(input_qnty.value);
+
+    if (itemName && quantity) {
+        addItem(itemName, quantity);
+        input_aria.value = '';
+        input_qnty.value = '';
+    }
+});
