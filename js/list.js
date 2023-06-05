363496a63217a110c0a8ca3da1124ec5c3490dd9
justanormaldog-disc <129561053+justanormaldog-disc@users.noreply.github.com>
2023-06-05 20:50:35 +1000

    Update list.js

--- js/list.js
+++ js/list.js
@@ -2,35 +2,46 @@ var input_aria = document.getElementById("input-aria");
 var input_button = document.getElementById("radius");
 var input_qnty = document.getElementById("qnty-aria");
 var table = document.getElementById('result');
+var table_element = document.getElementsByTagName("tr");
 
-var shop_list = [];
+var shop_list = []; // Declare shop_list variable outside initList()
 
 function initList() {
     shop_list = [];
+    console.log(input_aria);
 }
 
-function addItem(itemName, quantity) {
-    var lowercaseItemName = itemName.toLowerCase();
-    var foundItem = false;
-
-    for (var i = 0; i < shop_list.length; i++) {
-        var item = shop_list[i];
-        if (item[0].toLowerCase() === lowercaseItemName) {
-            item[1] += quantity;
-            foundItem = true;
-            break;
+function CAUitem(itemName, quantity) {
+    for (let i = 0; i < shop_list.length; i++) {
+        if (shop_list[i][0].toLowerCase() === itemName.toLowerCase()) {
+            shop_list[i][1] += quantity;
+            displayArray(shop_list);
+            return;
         }
     }
-
-    if (!foundItem) {
-        shop_list.push([itemName, quantity]);
-    }
-
+    // If item is not found, add it to the shopping list
+    shop_list.push([itemName, quantity]);
     displayArray(shop_list);
 }
 
+initList();
+
+input_button.addEventListener("click", function() {
+    if (input_aria.value !== "" && input_qnty.value !== "") {
+        CAUitem(input_aria.value, parseFloat(input_qnty.value, 10));
+        //clears textbox(s)
+        input_aria.value = "";
+        input_qnty.value = "";
+    }
+});
+
+//concentates and displays the array
 function displayArray(arr) {
-    table.innerHTML = ''; // Clear the table
+    // Clear existing rows with id="tab"
+    var tabRows = document.querySelectorAll('#result tr[id="tab"]');
+    tabRows.forEach(function(row) {
+        row.parentNode.removeChild(row);
+    });
 
     for (var i = 0; i < arr.length; i++) {
         var item = arr[i];
@@ -42,43 +53,31 @@ function displayArray(arr) {
         var cell3 = row.insertCell(2);
         var del = document.createElement("input");
         del.type = "button";
-        del.className = "del-btn";
+        del.setAttribute('id', 'del-btn'); 
+        row.setAttribute("class", "tab");
         del.value = 'Delete';
         cell1.innerHTML = name;
         cell2.innerHTML = value;
         cell3.appendChild(del);
-        del.setAttribute("data-index", i);
+        del.setAttribute("data-index", i); // Use data-index to store the index of the item
+        //event listener
         del.addEventListener("click", function() {
             var index = parseInt(this.getAttribute("data-index"));
-            shop_list.splice(index, 1);
-            displayArray(shop_list);
+            var row = this.parentNode.parentNode;
+            if (row.tagName === 'TR') {
+                table.deleteRow(row.rowIndex);
+                shop_list.splice(index, 1);
+                updateIndices(); // Update the indices after deletion
+            }
         });
     }
+  
+    updateIndices(); // Update the indices initially
 }
 
-function init() {
-    document.getElementById("name").innerHTML = 'Hello, ' + user;
-    input_aria.placeholder = aria[Math.floor(Math.random() * aria.length)] + "...";
-    document.getElementById("comment").innerHTML = item;
-    createComment();
-}
-
-function createComment() {
-    item = COMMENTS[Math.floor(Math.random() * COMMENTS.length)];
-    document.getElementById("comment").innerHTML = item;
-}
-
-init();
-initList();
-
-// Event listener for the button click
-input_button.addEventListener("click", function() {
-    var itemName = input_aria.value;
-    var quantity = parseInt(input_qnty.value);
-
-    if (itemName && quantity) {
-        addItem(itemName, quantity);
-        input_aria.value = '';
-        input_qnty.value = '';
+function updateIndices() {
+    var delButtons = document.getElementsByClassName('del-btn');
+    for (var i = 0; i < delButtons.length; i++) {
+        delButtons[i].setAttribute("data-index", i);
     }
-});
+}
