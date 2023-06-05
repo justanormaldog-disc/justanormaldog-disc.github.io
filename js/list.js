36ddc4ef7e16ee8bf371ca8a4c47102e78997954
justanormaldog-disc <129561053+justanormaldog-disc@users.noreply.github.com>
2023-06-05 20:31:39 +1000

    Update list.js

--- js/list.js
+++ js/list.js
@@ -7,69 +7,67 @@ var table_element = document.getElementsByTagName("tr");
 var shop_list = [];
 
 function initList() {
-    shop_list = [];
+  shop_list = [];
 }
 
 function addItem(itemName, quantity) {
-    var lowercaseItemName = itemName.toLowerCase();
-    var foundItem = false;
+  var lowercaseItemName = itemName.toLowerCase();
+  var foundItem = false;
 
-    for (var i = 0; i < shop_list.length; i++) {
-        var item = shop_list[i];
-        if (item[0].toLowerCase() === lowercaseItemName) {
-            item[1] += quantity;
-            foundItem = true;
-            break;
-        }
+  for (var i = 0; i < shop_list.length; i++) {
+    var item = shop_list[i];
+    if (item[0].toLowerCase() === lowercaseItemName) {
+      item[1] += quantity;
+      foundItem = true;
+      break;
     }
+  }
 
-    if (!foundItem) {
-        shop_list.push([itemName, quantity]);
-    }
+  if (!foundItem) {
+    shop_list.push([itemName, quantity]);
+  }
 
-    displayArray(shop_list);
+  displayArray(shop_list);
 }
 
 function displayArray(arr) {
-    var rows = table.getElementsByTagName('tr');
-    while (rows.length > 1) {
-        table.removeChild(rows[1]);
-    }
+  while (table.rows.length > 1) {
+    table.deleteRow(1);
+  }
 
-    for (var i = 0; i < arr.length; i++) {
-        var item = arr[i];
-        var name = item[0];
-        var value = item[1];
-        var row = table.insertRow(-1);
-        var cell1 = row.insertCell(0);
-        var cell2 = row.insertCell(1);
-        var cell3 = row.insertCell(2);
-        var del = document.createElement("input");
-        del.type = "button";
-        del.className = "del-btn";
-        del.value = 'Delete';
-        cell1.innerHTML = name;
-        cell2.innerHTML = value;
-        cell3.appendChild(del);
-        del.setAttribute("data-index", i);
-        del.addEventListener("click", function() {
-            var index = parseInt(this.getAttribute("data-index"));
-            var row = this.parentNode.parentNode;
-            if (row.tagName === 'TR') {
-                table.deleteRow(row.rowIndex);
-                shop_list.splice(index, 1);
-                updateIndices();
-            }
-        });
-    }
+  for (var i = 0; i < arr.length; i++) {
+    var item = arr[i];
+    var name = item[0];
+    var value = item[1];
+    var row = table.insertRow(-1);
+    var cell1 = row.insertCell(0);
+    var cell2 = row.insertCell(1);
+    var cell3 = row.insertCell(2);
+    var del = document.createElement("input");
+    del.type = "button";
+    del.className = "del-btn";
+    del.value = 'Delete';
+    cell1.innerHTML = name;
+    cell2.innerHTML = value;
+    cell3.appendChild(del);
+    del.setAttribute("data-index", i);
+    del.addEventListener("click", function() {
+      var index = parseInt(this.getAttribute("data-index"));
+      var row = this.parentNode.parentNode;
+      if (row.tagName === 'TR') {
+        table.deleteRow(row.rowIndex);
+        shop_list.splice(index, 1);
+        updateIndices();
+      }
+    });
+  }
 
-    updateIndices();
+  updateIndices();
 }
 
 function updateIndices() {
-    var delButtons = document.getElementsByClassName('del-btn');
-    for (var i = 0; i < delButtons.length; i++) {
-        delButtons[i].setAttribute("data-index", i);
-    }
+  var delButtons = document.getElementsByClassName('del-btn');
+  for (var i = 0; i < delButtons.length; i++) {
+    delButtons[i].setAttribute("data-index", i);
+  }
 }
-
