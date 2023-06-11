0ed473d78f9ee82973b7e294fe5f21c180b0774e
justanormaldog-disc <129561053+justanormaldog-disc@users.noreply.github.com>
2023-06-11 11:01:04 +1000

    major changes

--- js/list.js
+++ js/list.js
@@ -4,25 +4,67 @@ var input_qnty = document.getElementById("qnty-aria");
 var table = document.getElementById('result');
 var table_element = document.getElementsByTagName("tr");
 
+//flags
+modal_checkbox = false;
 var shop_list = []; // Declare shop_list variable outside initList()
 
 function initList() {
     shop_list = [];
-    console.log(input_aria);
 }
 
-// revise CAUitem for bug fix
 function CAUitem(itemName, quantity) {
-    for (let i = 0; i < shop_list.length; i++) {
+    for (var i = 0; i < shop_list.length; i++) {
+        var modal = document.getElementsByClassName("modal");
+        var modalP = document.getElementById("modal-p");
+        var nosee = document.getElementById("nosee");
         if (shop_list[i][0].toLowerCase() === itemName.toLowerCase()) {
-            shop_list[i][1] += quantity;
+            if (modal_checkbox == false) {
+            modalP.innerHTML = "An item already exists with the same name. If you continue, " + quantity + " will be added onto the item '" + itemName + "'s quantity.";
+            modal[0].style.display = "block";
+            modal[0].style.zIndex = 9999;
+            modal[0].style.top = "50%"; // Adjust the position based on your modal design
+            modal[0].style.left = "50%"; // Adjust the position based on your modal design
+            nosee.style.display = "block";
+            iterate = i;
+            qnty = quantity;
+            }else {
+                shop_list[i][1] += quantity;
+            }
             displayArray(shop_list);
             return;
+
         }
     }
     // If item is not found, add it to the shopping list
     shop_list.push([itemName, quantity]);
     displayArray(shop_list);
+}
+// modal button functions
+function btnOk() {
+    var modal = document.getElementsByClassName("modal");
+    var nosee = document.getElementById("nosee");
+    modal[0].style.display = "none";
+    nosee.style.display = "none";
+    shop_list[iterate][1] += qnty;
+    displayArray(shop_list);
+    checkbox();
+}
+
+function btnCancel() {
+    var modal = document.getElementsByClassName("modal");
+    var nosee = document.getElementById("nosee");
+    modal[0].style.display = "none";
+    nosee.style.display = "none";
+    displayArray(shop_list);
+    checkbox();
+}
+
+//checks if the checkbox is ticked
+function checkbox() {
+    var checkbox = document.getElementById("modal-checkbox");
+    if (checkbox.checked == true) {
+        modal_checkbox = true;
+    }
 }
 //
 initList();
@@ -33,6 +75,8 @@ input_button.addEventListener("click", function() {
         //clears textbox(s)
         input_aria.value = "";
         input_qnty.value = "";
+    }else {
+        alertPopup();
     }
 });
 
@@ -57,7 +101,6 @@ function displayArray(arr) {
         row.setAttribute("class", "tab");
         del.value = 'Delete';
         cell1.innerHTML = name;
-        cell1.setAttribute("class", "cell-name");
         cell2.innerHTML = value;
         cell3.appendChild(del);
         del.setAttribute("data-index", i); // Use data-index to store the index of the item
@@ -81,4 +124,41 @@ function updateIndices() {
     for (var i = 0; i < delButtons.length; i++) {
         delButtons[i].setAttribute("data-index", i);
     }
+}
+
+//search box
+
+function searchItems() {
+    var rows, searchValue
+    rows = document.getElementsByClassName("tab");
+    for (var i = 0; i < rows.length; i++) {
+        searchValue = document.getElementById("search-bar").value;
+        if (rows[i].childNodes[0].innerHTML.indexOf(searchValue) > -1) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+
+}
+
+function alertPopup() {
+    var x = 100;
+    var y = 0;
+    var div = document.createElement("div");
+    div.setAttribute('class', 'alert');
+    div.innerHTML = "Invalid Input! Please enter a value into both input boxes.";
+    document.getElementById("container").appendChild(div);
+
+    setTimeout(function() {
+        var intervalId = setInterval(function() {
+            x -= 2;
+            y += 1;
+            div.style.opacity = x / 100;
+            if (y === 50) {
+                div.remove();
+                clearInterval(intervalId);
+            }
+        }, 1);
+    }, 2500);
 }
